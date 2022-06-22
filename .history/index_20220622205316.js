9cbc2ab0ddf4c53a767d854e34affbb288783f0a
peterclermy <peterclermy232@gmail.com>
2022-06-22 20:56:11 +0300

    created class LocalCart and use static method to get and add the items by use of JSON.stringify,mapping

---
+++ .history/index_20220622205316.js
@@ -0,0 +1,9 @@
+class CartItem{
+    constructor(name, desc, img, price){
+        this.name = name
+        this.desc = desc
+        this.img=img
+        this.price = price
+        this.quantity = 1
+   }
+}
