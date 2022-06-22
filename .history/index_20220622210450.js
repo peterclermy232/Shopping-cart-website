930256f25ec1c7644c322462ccc7ac04967a1e46
peterclermy <peterclermy232@gmail.com>
2022-06-22 21:08:00 +0300

    initialize cartIcon,WholeCart, addToCartBtns and event listner

---
+++ .history/index_20220622210450.js
@@ -0,0 +1,55 @@
+class CartItem{
+    constructor(name, desc, img, price){
+        this.name = name
+        this.desc = desc
+        this.img=img
+        this.price = price
+        this.quantity = 1
+   }
+}
+
+class LocalCart{
+    static key = "cartItems"
+
+    static getLocalCartItems(){
+        let cartMap = new Map()
+     const cart = localStorage.getItem(LocalCart.key)   
+     if(cart===null || cart.length===0)  return cartMap
+        return new Map(Object.entries(JSON.parse(cart)))
+    }
+
+    static addItemToLocalCart(id, item){
+        let cart = LocalCart.getLocalCartItems()
+        if(cart.has(id)){
+            let mapItem = cart.get(id)
+            mapItem.quantity +=1
+            cart.set(id, mapItem)
+        }
+        else
+        cart.set(id, item)
+       localStorage.setItem(LocalCart.key,  JSON.stringify(Object.fromEntries(cart)))
+       updateCartUI()
+        
+    }
+
+    static removeItemFromCart(id){
+        let cart = LocalCart.getLocalCartItems()
+        if(cart.has(id)){
+            let mapItem = cart.get(id)
+            if(mapItem.quantity>1)
+           {
+            mapItem.quantity -=1
+            cart.set(id, mapItem)
+           }
+           else
+           cart.delete(id)
+        } 
+        if (cart.length===0)
+        localStorage.clear()
+        else
+        localStorage.setItem(LocalCart.key,  JSON.stringify(Object.fromEntries(cart)))
+           updateCartUI()
+        }
+    }
+
+    
